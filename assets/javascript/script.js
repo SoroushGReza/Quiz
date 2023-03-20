6e75988d2bdbf1bdfc0875b27a0f205589331740
SoroushGReza <mycodeschool22@outlook.com>
2023-03-20 10:08:21 +0000

    ADD: Structure of Rules dropdown in index.html, script.js & style.css

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+/* Toggle between hiding / showing the dropdown content */
+function rulesDropDown() {
+    document.getElementById("dropRulesContent").classList.toggle("show");
+}
+
+// Close the dropdown if user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
