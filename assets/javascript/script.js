00632f6a071da365b4f3fea1d53cac0ee371335f
SoroushGReza <mycodeschool22@outlook.com>
2023-03-22 10:41:20 +0000

    Add: Structure to the questions in script.js, And randomize the questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,58 @@
-let startQuiz = document.getElementById("startQuiz");
-let question = document.getElementById("question");
+let startQuiz = document.getElementById("start-quiz");
+let questionContainer = document.getElementById("question-container");
+const answers = document.getElementsByClassName('answer-button');
+let currentQuestionIndex = 0;
+let currentQuestion = {};
+let questions = [
+    {
+        question: "What does HTML stand for?", answers: ["A. Hyper Text Markup Language", "Hot Tamale Marmelade Lollipop", "The Hail Mighty Lobster"]
+    }, 
+    {
+        question: "What does HTML stand for? 2", answers: ["A. Hyper Text Markup Language", "Hot Tamale Marmelade Lollipop", "The Hail Mighty Lobster"]
+    }, 
+    {
+        question: "What does HTML stand for? 3", answers: ["A. Hyper Text Markup Language", "Hot Tamale Marmelade Lollipop", "The Hail Mighty Lobster"]
+    }, 
+    {
+        question: "What does HTML stand for? 4", answers: ["A. Hyper Text Markup Language", "Hot Tamale Marmelade Lollipop", "The Hail Mighty Lobster"]
+    }, 
+    {
+        question: "What does HTML stand for? 5", answers: ["A. Hyper Text Markup Language", "Hot Tamale Marmelade Lollipop", "The Hail Mighty Lobster"]
+    }
+];
+
+function getNextQuestion(){
+    currentQuestionIndex = Math.floor(Math.random() * (questions.length - 1)) ;
+    currentQuestion = questions[currentQuestionIndex];
+}
+
+
+function displayQuestion(questionObject){
+    document.getElementById('question').innerText = questionObject.question;
+    document.getElementById('button0').innerText = questionObject.answers[0];
+    document.getElementById('button1').innerText = questionObject.answers[1];
+    document.getElementById('button2').innerText = questionObject.answers[2];
+}
+
+
+
 
 startQuiz.addEventListener("click", function() {
     startQuiz.style.display = "none"; 
-    question.style.display = "block";
+    questionContainer.style.display = "block";
+    getNextQuestion();
+    displayQuestion(currentQuestion);
 });
+
+for (var i = 0; i < answers.length; i++) {
+    const answer = answers[i];
+    answer.addEventListener('click', function(){
+        // Check the answer and update score
+        // avoid repeated questions
+        getNextQuestion();
+        displayQuestion(currentQuestion);
+
+    });
+}
+
+
