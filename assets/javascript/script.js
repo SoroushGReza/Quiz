6f7fc55665de7c6eec62749a43d0f74cf7aed029
SoroushGReza <mycodeschool22@outlook.com>
2023-03-22 15:35:24 +0000

    Add: Variables: 'currentQuestion' and 'score', Functions: 'loadQuestion' and 'checkAnswer' in script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,92 +5,110 @@ let currentQuestionIndex = 0;
 let currentQuestion = {};
 
 /* Questions and answers */
-let questions = [
-    {
-        question: "What does HTML stand for?", answers: ["Hyper Text Markup Language", "Hot Tamale Marmelade Lollipop", "Hail The Mighty Lobster"],
+let questions = [{
+        question: "What does HTML stand for?",
+        answers: ["Hyper Text Markup Language", "Hot Tamale Marmelade Lollipop", "Hail The Mighty Lobster"],
         correctAnswer: 0
-    }, 
+    },
     {
-        question: "What is purpose of the <head> tag in HTML?", answers: ["To display the heading of the page", "To contain metadata about the document", "To add a musical headbang animation to the page"],
+        question: "What is purpose of the <head> tag in HTML?",
+        answers: ["To display the heading of the page", "To contain metadata about the document", "To add a musical headbang animation to the page"],
         correctAnswer: 1
-    }, 
+    },
     {
-        question: "How do you create a hyperlink in HTML?", answers: ["By using the <a> tag", "By using the <h> tag", "By using the <i> tag"],
+        question: "How do you create a hyperlink in HTML?",
+        answers: ["By using the <a> tag", "By using the <h> tag", "By using the <i> tag"],
         correctAnswer: 0
-    }, 
-    { 
-        question: "What is the difference between an ordered list and an unordered list in HTML?", answers: ["Unordered lists have numbers or letters, while ordered lists have bullet points", "There is no difference, they are the same thing in HTML", "Ordered lists have numbers or letters, while unordered lists have bullet points"],
-        correctAnswer: 2
-    }, 
+    },
     {
-        question: "What does the <img> tag do in HTML?", answers: ["Adds a music player to the webpage", "Displays an image on the webpage", "Creates a rainbow-colored text effect on the webpage"],
+        question: "What is the difference between an ordered list and an unordered list in HTML?",
+        answers: ["Unordered lists have numbers or letters, while ordered lists have bullet points", "There is no difference, they are the same thing in HTML", "Ordered lists have numbers or letters, while unordered lists have bullet points"],
+        correctAnswer: 2
+    },
+    {
+        question: "What does the <img> tag do in HTML?",
+        answers: ["Adds a music player to the webpage", "Displays an image on the webpage", "Creates a rainbow-colored text effect on the webpage"],
         correctAnswer: 1
-    }, 
+    },
     {
-        question: "What does CSS stand for? ", answers: ["Cute and Snuggly Sloths", "Crispy Spicy Salmon", "Cascading Style Sheets"],
+        question: "What does CSS stand for? ",
+        answers: ["Cute and Snuggly Sloths", "Crispy Spicy Salmon", "Cascading Style Sheets"],
         correctAnswer: 2
-    }, 
-    { 
-        question: "How do you change the font size of an element in CSS?", answers: ["By using the font-size property", "By using the text-size property", "By using the word-size property"],
+    },
+    {
+        question: "How do you change the font size of an element in CSS?",
+        answers: ["By using the font-size property", "By using the text-size property", "By using the word-size property"],
         correctAnswer: 0
-    }, 
-    {     
-        question: "What is the purpose of the box-sizing property in CSS?", answers: ["To control how the width and height of an element are calculated", "To add a border to the element", "To change the background color of the element to pink"],
+    },
+    {
+        question: "What is the purpose of the box-sizing property in CSS?",
+        answers: ["To control how the width and height of an element are calculated", "To add a border to the element", "To change the background color of the element to pink"],
         correctAnswer: 0
-    }, 
-    { 
-        question: "How do you add a background color to an element in CSS?", answers: ["By using the color property", "By using the foreground-color property", "By using the background-color property"],
+    },
+    {
+        question: "How do you add a background color to an element in CSS?",
+        answers: ["By using the color property", "By using the foreground-color property", "By using the background-color property"],
         correctAnswer: 2
-    }, 
-    { 
-        question: "How do you create a class in CSS?", answers: ["By using the #classname selector", "By using the .classname selector", "By using the @classname selector"],
+    },
+    {
+        question: "How do you create a class in CSS?",
+        answers: ["By using the #classname selector", "By using the .classname selector", "By using the @classname selector"],
         correctAnswer: 1
-    }, 
+    },
     {
-        question: "What is the purpose of JavaScript?", answers: ["To make coffee", "To add interactivity and dynamic effects to web pages", "To teach dogs how to code"],
+        question: "What is the purpose of JavaScript?",
+        answers: ["To make coffee", "To add interactivity and dynamic effects to web pages", "To teach dogs how to code"],
         correctAnswer: 1
-    }, 
+    },
     {
-        question: "How do you declare a variable in JavaScript?", answers: ["By using the var keyword", "By using the const keyword", "By using the let keyword"],
+        question: "How do you declare a variable in JavaScript?",
+        answers: ["By using the var keyword", "By using the const keyword", "By using the let keyword"],
         correctAnswer: 0
-    }, 
+    },
     {
-        question: "What is the difference between == and === in JavaScript?", answers: ["== always returns true, while === always returns false", "There is no difference, they are the same thing in JavaScript", "== compares values, while === compares values and types"],
+        question: "What is the difference between == and === in JavaScript?",
+        answers: ["== always returns true, while === always returns false", "There is no difference, they are the same thing in JavaScript", "== compares values, while === compares values and types"],
         correctAnswer: 2
-    }, 
+    },
     {
-        question: "How do you create a function in JavaScript?", answers: ["By using the function keyword", "By using the method keyword", "By using the class keyword"],
+        question: "How do you create a function in JavaScript?",
+        answers: ["By using the function keyword", "By using the method keyword", "By using the class keyword"],
         correctAnswer: 0
-    }, 
+    },
     {
-        question: "What does the if statement do in JavaScript?", answers: ["It makes your code more difficult to read", "It executes code if a specified condition is true", "It sends an email to your grandma"],
+        question: "What does the if statement do in JavaScript?",
+        answers: ["It makes your code more difficult to read", "It executes code if a specified condition is true", "It sends an email to your grandma"],
         correctAnswer: 1
-    }, 
+    },
     {
-        question: "What is Python? ", answers: ["A type of snake", "A type of fruit", "A high-level programming language"],
+        question: "What is Python? ",
+        answers: ["A type of snake", "A type of fruit", "A high-level programming language"],
         correctAnswer: 2
-    }, 
-    { 
-        question: "How do you print something to the console in Python?", answers: ["By using the print() function", "By using the display() function", "By using the shout() function"],
+    },
+    {
+        question: "How do you print something to the console in Python?",
+        answers: ["By using the print() function", "By using the display() function", "By using the shout() function"],
         correctAnswer: 0
-    }, 
-    {   
-        question: "What is the purpose of a for loop in Python?", answers: ["To output a random number", "To count how many fingers you have", "To execute a block of code repeatedly for a specified number of times"],
-        correctAnswer: 2
-    }, 
+    },
     {
-        question: "How do you read input from the user in Python?", answers: ["By using the read() function", "By using the input() function", "By using the listen() function"],
+        question: "What is the purpose of a for loop in Python?",
+        answers: ["To output a random number", "To count how many fingers you have", "To execute a block of code repeatedly for a specified number of times"],
+        correctAnswer: 2
+    },
+    {
+        question: "How do you read input from the user in Python?",
+        answers: ["By using the read() function", "By using the input() function", "By using the listen() function"],
         correctAnswer: 1
-    }, 
+    },
 ];
 
-function getNextQuestion(){
-    currentQuestionIndex = Math.floor(Math.random() * (questions.length - 1)) ;
+function getNextQuestion() {
+    currentQuestionIndex = Math.floor(Math.random() * (questions.length - 1));
     currentQuestion = questions[currentQuestionIndex];
 }
 
 
-function displayQuestion(questionObject){
+function displayQuestion(questionObject) {
     document.getElementById('question').innerText = questionObject.question;
     document.getElementById('button0').innerText = questionObject.answers[0];
     document.getElementById('button1').innerText = questionObject.answers[1];
@@ -99,8 +117,8 @@ function displayQuestion(questionObject){
 
 
 
-startQuiz.addEventListener("click", function() {
-    startQuiz.style.display = "none"; 
+startQuiz.addEventListener("click", function () {
+    startQuiz.style.display = "none";
     questionContainer.style.display = "block";
     getNextQuestion();
     displayQuestion(currentQuestion);
@@ -108,13 +126,32 @@ startQuiz.addEventListener("click", function() {
 
 for (var i = 0; i < answers.length; i++) {
     const answer = answers[i];
-    answer.addEventListener('click', function(){
+    answer.addEventListener('click', function () {
         // Check the answer and update score
-        // avoid repeated questions
+let currentQuestion = 0;
+let score = 0;
+
+function loadQuestion() {
+    const question = quizData[currentQuestion];
+
+    document.getElementById("questionNum").textContent = currentQuestion + 1;
+    document.getElementById("question").textContent = question.question;
+    document.getElementById("button0").textContent = question.answers[0];
+    document.getElementById("button1").textContent = question.answers[1];
+    document.getElementById("button2").textContent = question.answers[2];
+}
+
+function CheckAnswer(answer) {
+    const question = quizData[currentQuestion];
+
+    if (answer === question.correct) {
+        score++;
+        document.getElementsByClassName("score")[0].textContent = "Right Answers: " + score;
+    }
+}
+// avoid repeated questions
         getNextQuestion();
         displayQuestion(currentQuestion);
 
     });
 }
-
-
